0a3b61e0d28a05761025bf402070ac1bc1fb1693
mj <develmj@gmail.com>
2016-11-15 23:44:50 +0530

    Added base calls

---
+++ app/controllers/otcarp_controller.rb
@@ -0,0 +1,27 @@
+class OtcarpController < ApplicationController
+
+  def add_movie
+  end
+
+  def search_for_movie
+  end
+
+  def add_cast_to_movie
+  end
+
+  def add_crew_to_movie
+  end
+
+  def add_user
+  end
+
+  def search_for_user
+  end
+
+  def get_movie_ratings
+  end
+
+  def get_movie_reviews
+  end
+
+end
---
+++ app/controllers/userpart_controller.rb
@@ -0,0 +1,21 @@
+class UserpartController < ApplicationController
+
+  def add_review_to_movie
+  end
+
+  def get_rate_movie
+  end
+
+  def get_recommended_movies
+  end
+
+  def get_recommended_movies_for
+  end
+
+  def upvote_review
+  end
+
+  def downvote_review
+  end
+
+end
